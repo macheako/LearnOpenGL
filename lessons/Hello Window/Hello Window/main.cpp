08cf4ba082b05cd793b533133d6b44c6967708f6
Crunchy <crunchy@Crunchys-MacBook-Pro.local>
2020-05-21 12:50:48 -0400

    Setup Repository

---
+++ lessons/Hello Window/Hello Window/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello Window
+//
+//  Created by Crunchy on 5/21/20.
+//  Copyright © 2020 AnOrganization. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
